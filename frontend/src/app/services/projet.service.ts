ae7b7ad00c83ddb3a93efe755d6170a9de991d8a
Valerdi <75272925+AlexiaValerdi@users.noreply.github.com>
2020-12-02 10:18:41 -0600

    Update projet.service.ts

--- frontend/src/app/services/projet.service.ts
+++ frontend/src/app/services/projet.service.ts
@@ -24,7 +24,10 @@ import { Global } from './global';
         }
 
         // Aqui va getEjemplar ...
-
+        getEjemplar(): Observable<any>{
+          let headers = new HttpHeaders().set('Content-Type', 'application/json');
+          return this._http.get(this.url+'getEjemplar',{headers: headers});
+        }
         // Aqui va eliminarEjemplar...
 
     }
